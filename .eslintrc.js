4775f380ae688e9802be232712aa5fcb70c9e566
Jamie Ferguson <git@jppferguson.com>
2020-01-30 22:12:51 +0000

    Add plugins and make rules easier to read

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,29 @@
+// I always forget these, so make them more readable!
+const OFF = "off";   // "off" or 0
+const WARN = "warn";   // "warn" or 1
+const ERROR = "error"; // "error" or 2
+
 module.exports = {
   "extends": [
     "airbnb",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/typescript",
     "prettier",
-    "prettier/react"
+    "prettier/react",
+    "prettier/@typescript-eslint",
   ],
-  "parser": "babel-eslint",
+  "plugins": [
+    "html",
+    "prettier",
+    "react-hooks",
+    "import",
+    "module-resolver",
+    "simple-import-sort",
+    "@typescript-eslint",
+  ],
+  "parser": "@typescript-eslint/parser",
   "parserOptions": {
     "ecmaVersion": 2020,
     // Can I remove these now?
@@ -20,68 +39,68 @@ module.exports = {
     "jest": true
   },
   "rules": {
-    "no-debugger": 0,
-    "no-alert": 0,
-    "no-await-in-loop": 0,
+    "no-debugger": OFF,
+    "no-alert": OFF,
+    "no-await-in-loop": OFF,
     "no-return-assign": [
-      "error",
+      ERROR,
       "except-parens"
     ],
     "no-restricted-syntax": [
-      2,
+      ERROR,
       "ForInStatement",
       "LabeledStatement",
       "WithStatement"
     ],
     "no-unused-vars": [
-      1,
+      WARN,
       {
         "ignoreSiblings": true,
         "argsIgnorePattern": "res|next|^err"
       }
     ],
     "prefer-const": [
-      "error",
+      ERROR,
       {
         "destructuring": "all",
       }
     ],
     "arrow-body-style": [
-      2,
+      ERROR,
       "as-needed"
     ],
     "no-unused-expressions": [
-      2,
+      ERROR,
       {
         "allowTaggedTemplates": true
       }
     ],
     "no-param-reassign": [
-      2,
+      ERROR,
       {
         "props": false
       }
     ],
-    "no-console": 0,
-    "import/prefer-default-export": 0,
-    "import": 0,
-    "func-names": 0,
-    "space-before-function-paren": 0,
-    "comma-dangle": 0,
-    "max-len": 0,
-    "import/extensions": 0,
-    "no-underscore-dangle": 0,
-    "consistent-return": 0,
-    "react/display-name": 1,
-    "react/no-array-index-key": 0,
-    "react/react-in-jsx-scope": 0,
-    "react/prefer-stateless-function": 0,
-    "react/forbid-prop-types": 0,
-    "react/no-unescaped-entities": 0,
-    "jsx-a11y/accessible-emoji": 0,
-    "react/require-default-props": 0,
+    "no-console": OFF,
+    "import/prefer-default-export": OFF,
+    "import": OFF,
+    "func-names": OFF,
+    "space-before-function-paren": OFF,
+    "comma-dangle": OFF,
+    "max-len": OFF,
+    "import/extensions": OFF,
+    "no-underscore-dangle": OFF,
+    "consistent-return": OFF,
+    "react/display-name": WARN,
+    "react/no-array-index-key": OFF,
+    "react/react-in-jsx-scope": OFF,
+    "react/prefer-stateless-function": OFF,
+    "react/forbid-prop-types": OFF,
+    "react/no-unescaped-entities": OFF,
+    "jsx-a11y/accessible-emoji": OFF,
+    "react/require-default-props": OFF,
     "react/jsx-filename-extension": [
-      1,
+      WARN,
       {
         "extensions": [
           ".js",
@@ -89,9 +108,9 @@ module.exports = {
         ]
       }
     ],
-    "radix": 0,
+    "radix": OFF,
     "no-shadow": [
-      2,
+      ERROR,
       {
         "hoist": "all",
         "allow": [
@@ -105,7 +124,7 @@ module.exports = {
       }
     ],
     "quotes": [
-      2,
+      ERROR,
       "single",
       {
         "avoidEscape": true,
@@ -113,28 +132,25 @@ module.exports = {
       }
     ],
     "prettier/prettier": [
-      "error",
+      ERROR,
       {
         "trailingComma": "es5",
         "singleQuote": true,
         "printWidth": 80,
       }
     ],
-    "jsx-a11y/href-no-hash": "off",
+    "jsx-a11y/href-no-hash": OFF,
     "jsx-a11y/anchor-is-valid": [
-      "warn",
+      WARN,
       {
         "aspects": [
           "invalidHref"
         ]
       }
     ],
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn"
+    "react-hooks/rules-of-hooks": ERROR,
+    "react-hooks/exhaustive-deps": WARN
   },
-  "plugins": [
-    "html",
-    "prettier",
-    "react-hooks"
-  ]
+  "settings": {
+  }
 }
